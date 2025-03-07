dbee3b5b23d5d5201036dfce4eaef5144f02c95c
David <eversonrestrepo@unicauca.edu.co>
2025-03-06 20:55:21 -0500

    modificacion de repositorioStudent

--- SoftwareProjectManagement/src/main/java/co/edu/unicauca/access/StudentMySQLRepository.java
+++ SoftwareProjectManagement/src/main/java/co/edu/unicauca/access/StudentMySQLRepository.java
@@ -6,7 +6,12 @@ package co.edu.unicauca.access;
 
 import co.edu.unicauca.interfaces.IStudentRepository;
 import co.edu.unicauca.domain.entities.Student;
+import co.edu.unicauca.infra.Messages;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -15,16 +20,42 @@ import java.util.List;
 public class StudentMySQLRepository implements IStudentRepository{
 
 
+    
 
     @Override
     public boolean delete(int id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-
     @Override
-    public boolean save(Object entity) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public boolean save(Object usuario) {
+        Student estudiante = (Student) usuario;
+        Connection conexion = Conectionbd.conectar(); // Conexión a la BD
+
+        if (conexion == null) {
+             Messages.showMessageDialog("Error de conexion", "Atención");
+            return false;
+        }
+
+        try {
+            String sql = "{CALL sp_registrar_estudiante(?, ?, ?, ?, ?)}";
+            CallableStatement stmt = conexion.prepareCall(sql);
+
+            stmt.setInt(1, estudiante.getCodigo());
+            stmt.setString(2, estudiante.getNombre());
+            stmt.setString(3, estudiante.getCedula());
+            stmt.setString(4, estudiante.getEmail());
+            stmt.setString(5, estudiante.getTelefono());
+
+            stmt.execute();
+            stmt.close();
+            conexion.close();
+
+            return true;  // Registro exitoso
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al registrar estudiante: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            return false;  // Hubo un error
+        }
     }
 
     @Override
